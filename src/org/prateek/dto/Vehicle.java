daaf8840953058e4a2cc3f016b8e5398826a9b33
prateeka <prateeka@hp.com>
2014-04-05 13:20:02 -0700

    Table per class strategy

--- src/org/prateek/dto/Vehicle.java
+++ src/org/prateek/dto/Vehicle.java
@@ -7,11 +7,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 /**
  * 
  */
 @Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Vehicle
 {
     @Id
